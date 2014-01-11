69df33df2442cef52c30b9b2e951379d6250206a
Alex Lawson <cheswick13@hotmail.com>
2014-01-10 19:06:51 -0800

    worked on ejector and hopper (but they still don't work)

--- objects/wired/pipe/ejector/ejector.lua
+++ objects/wired/pipe/ejector/ejector.lua
@@ -6,3 +6,16 @@ end
 function main(args)
   pipes.update(entity.dt())
 end
+
+function onItemPut(itemList)
+  local position = entity.position()
+  for _, item in pairs(itemList) do
+    world.logInfo("Putting item %s", item[3])
+    if next(item[3]) == nil then 
+      world.spawnItem(item[1], {position[1] + 0.5, position[2] - 0.5}, item[2])
+    else
+      world.spawnItem(item[1], {position[1] + 0.5, position[2] - 0.5}, item[2], item[3])
+    end
+  end
+  return true
+end
--- objects/wired/pipe/hopper/hopper.lua
+++ objects/wired/pipe/hopper/hopper.lua
@@ -1,8 +1,30 @@
 function init(virtual)
-  pipes.init({itemPipe})
+  if not virtual then
+    pipes.init({itemPipe})
+  end
 end
 
 --------------------------------------------------------------------------------
 function main(args)
   pipes.update(entity.dt())
+
+  local itemDropList = findItemDrops()
+  if #itemDropList > 0 then
+    world.logInfo(itemDropList)
+    local itemList = {}
+    for i, itemId in ipairs(itemDropList) do
+      itemList[i] = world.takeItemDrop(itemId)
+    end
+    world.logInfo(itemList)
+
+    if #itemList > 0 then
+      local result = pushItem(1, itemList)
+      world.logInfo(result)
+    end
+  end
+end
+
+function findItemDrops()
+  local pos = entity.position()
+  return world.itemDropQuery(pos, {pos[1] + 2, pos[2] + 1})
 end
