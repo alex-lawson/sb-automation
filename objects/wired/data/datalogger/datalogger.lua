fc02e0d13de8042e323403970092e3a2792d69e4
agentsnazz@gmail.com <agentsnazz@gmail.com>
2014-01-10 20:05:36 -0500

    datalogger animation in progress

--- objects/wired/data/datalogger/datalogger.lua
+++ objects/wired/data/datalogger/datalogger.lua
@@ -4,57 +4,35 @@ function init(virtual)
       storage.enabled = true
     end
 
-    -- what do?
-    self.flipStr = ""
-    if entity.direction() == -1 then
-      self.flipStr = "flipped."
-    end
-
-    updateAnimationState()
-
+    entity.setAnimationState("loggerState", "disabled")
     datawire.init()
   end
 end
 
--- Done
 function onInteraction(args)
-  storage.enabled ==  not storage.enabled
-end
-
-function onInboundNodeChange(args)
-  
-  output()
-  updateAnimationState()
-end
-
-
-function updateAnimationState()
+  storage.enabled =  not storage.enabled
   if storage.enabled then
-    -- set the animation state to glowing
+    entity.setAnimationState("loggerState", "enabled")
   else
-    -- set the animation state to dark
+    entity.setAnimationState("loggerState", "disabled")
   end
 end
 
--- Done
 function validateData(data, dataType, nodeId)
   --only receive data on node 0
   return nodeId == 0
 end
 
--- Done
 function onValidDataReceived(data, dataType, nodeId)
   if storage.enabled then
     logInfo(data .. dataType)
   end
 end
 
--- Done
 function logInfo(stringToLog)
   world.logInfo(stringToLog)
 end
 
--- Done
 function main()
   datawire.update()
 end
