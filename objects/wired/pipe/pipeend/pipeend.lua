b9742941af0b84b943a3ea998c8b7aa250c7eb11
Healthire <bananavice@gmail.com>
2014-01-13 15:24:50 +0100

    Pipe spout now functions in all 4 directions, but only one at a time

--- objects/wired/pipe/pipeend/pipeend.lua
+++ objects/wired/pipe/pipeend/pipeend.lua
@@ -2,6 +2,8 @@ function init(virtual)
   entity.setInteractive(true)
   
   pipes.init({liquidPipe})
+  
+  self.usedNode = 0
 end
 
 --------------------------------------------------------------------------------
@@ -13,9 +15,29 @@ end
 --------------------------------------------------------------------------------
 function main(args)
   pipes.update(entity.dt())
+  
+  local position = entity.position()
+  local checkDirs = {}
+  checkDirs[0] = {-1, 0}
+  checkDirs[1] = {0, -1}
+  checkDirs[2] = {1, 0}
+  checkDirs[3] = {0, 1}
+  
+  for i=0,3 do 
+    local angle = (math.pi / 2) * i
+    local tilePos = {position[1] + checkDirs[i][1], position[2] + checkDirs[i][2]}
+    local pipeDirections = pipes.getPipeDirections("liquid", tilePos)
+    if pipeDirections and pipes.pipesConnect(checkDirs[i], pipeDirections) then
+      entity.rotateGroup("pipe", angle)
+      self.usedNode = i + 1
+    end
+  end
+  
+  
 end
 
-function canGetLiquid(liquid)
+function canGetLiquid(liquid, nodeId)
+  if nodeId ~= self.usedNode then return false end
   --Only get liquid if the pipe is emerged in liquid
   local position = entity.position()
   local liquidPos = {position[1] + 0.5, position[2] + 0.5}
@@ -27,26 +49,27 @@ function canGetLiquid(liquid)
   return false
 end
 
-function canPutLiquid(liquid)
-  --Always put liquid. ALWAYS.
+function canPutLiquid(liquid, nodeId)
+  if nodeId ~= self.usedNode then return false end
+  
   return true
 end
 
-function onLiquidGet(liquid)
+function onLiquidGet(liquid, nodeId)
   local position = entity.position()
   local liquidPos = {position[1] + 0.5, position[2] + 0.5}
-  local liquid = world.liquidAt(liquidPos)
-  if liquid then
+  local getLiquid = canGetLiquid(liquid, nodeId)
+  if getLiquid then
     world.spawnProjectile("destroyliquid", liquidPos, entity.id(), {0, -1}, false, {speed = 100})
-    return liquid
+    return getLiquid
   end
   return false
 end
 
-function onLiquidPut(liquid)
+function onLiquidPut(liquid, nodeId)
   local position = entity.position()
   local liquidPos = {position[1] + 0.5, position[2] + 0.5}
-  if canPutLiquid(liquid) then
+  if canPutLiquid(liquid, nodeId) then
     world.spawnProjectile("createliquid", liquidPos, entity.id(), {0, -1}, false, {speed = 100, actionOnReap = { {action = "liquid", quantity = liquid[2], liquidId = liquid[1]}}})
     return true
   else
@@ -54,10 +77,10 @@ function onLiquidPut(liquid)
   end
 end
 
-function beforeLiquidGet(liquid)
-  return canGetLiquid(liquid)
+function beforeLiquidGet(liquid, nodeId)
+  return canGetLiquid(liquid, nodeId)
 end
 
-function beforeLiquidPut(liquid)
-  return canPutLiquid(liquid)
+function beforeLiquidPut(liquid, nodeId)
+  return canPutLiquid(liquid, nodeId)
 end
