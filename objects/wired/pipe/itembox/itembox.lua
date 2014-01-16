89cc8397a9898e43a9609a11bb135c3b2c49b3f0
Healthire <bananavice@gmail.com>
2014-01-16 22:20:24 +0100

    Fixed itembox issues Made ejector passive again

--- objects/wired/pipe/itembox/itembox.lua
+++ objects/wired/pipe/itembox/itembox.lua
@@ -3,23 +3,32 @@ function init(args)
   
   if args == false then
     pipes.init({itemPipe})
+    
+    local initInv = entity.configParameter("initialInventory")
+    if initInv and storage.sApi == nil then
+      storage.sApi = initInv
+    end
+    
     storageApi.init(3, 16, true)
-    entity.scaleGroup("invbar", {1, 1})
+    
+    entity.scaleGroup("invbar", {1, 0})
     
     if entity.direction() < 0 then
       entity.setAnimationState("flipped", "left")
     end
     
-    local initInv = entity.configParameter("initialInventory")
-    if initInv then
-      storage.sApi = initInv
-    end
+    self.pushRate = entity.configParameter("itemPushRate")
+    self.pushTimer = 0
   end
 end
 
 function die()
   local position = entity.position()
-  world.spawnItem("itembox", {position[1] + 1.5, position[2] + 1}, 1, {initialInventory = storage.sApi})
+  if storageApi.getCount() == 0 then
+    world.spawnItem("itembox", {position[1] + 1.5, position[2] + 1}, 1)
+  else
+    world.spawnItem("itembox", {position[1] + 1.5, position[2] + 1}, 1, {initialInventory = storage.sApi})
+  end
 end
 
 function main(args)
@@ -34,6 +43,18 @@ function main(args)
     entity.setAnimationState("invbar", "medium")
   else
     entity.setAnimationState("invbar", "full")
+  end
+  
+  --Push out items if switched on
+  if entity.getInboundNodeLevel(0) then
+    if self.pushTimer > self.pushRate then
+      for i,item in storageApi.getIterator() do
+        pushItem(2, storageApi.returnItem(i))
+        break
+      end
+      self.pushTimer = 0
+    end
+    self.pushTimer = self.pushTimer + entity.dt()
   end
 end
 
