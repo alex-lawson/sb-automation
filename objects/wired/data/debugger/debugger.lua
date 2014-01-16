6ff8571afe7960e4e262561dea3f450cd6034b00
agentsnazz <agentsnazz@gmail.com>
2014-01-16 16:08:11 -0500

    Debugger: The Debuggering

--- objects/wired/data/debugger/debugger.lua
+++ objects/wired/data/debugger/debugger.lua
@@ -1,10 +1,27 @@
-function init(args)
+function init(virtual)
+  if not virtual then
+    if storage.fingerpint == nil then
+      -- First Initialization
+      storage.fingerprint = entity.position()[1] .. "." ..entity.position()[2]
+      storage.name = storage.fingerprint
+      output(false)
+    else
+      -- Re-Initialization
+      output(storage.state)
+    end
 
-  if storage.state == nil then
-    output(false)
-  else
-    output(storage.state)
+    -- Every Initialization
+    datawire.init()
+    entity.setInteractive(true)
   end
+end
+
+function main(args)
+  datawire.update()
+end
+
+function onInteraction(args)
+  output(not storage.state)
 end
 
 function output(state)
@@ -12,16 +29,29 @@ function output(state)
     storage.state = state
     if state then
       entity.setAnimationState("debugState", "on")
+      logInfo("--- Enabling Logging ---")
     else
       entity.setAnimationState("debugState", "off")
+      logInfo("--- Disabling Logging ---")
     end
   end
 end
 
-function main(args)
-  if entity.getInboundNodeLevel(0) then
-    output(true)
-  else
-    output(false)
+function logInfo(stringToLog)
+  world.logInfo("Debugger " .. storage.name .. ": " .. stringToLog)
+end
+
+function validateData(data, dataType, nodeId)
+  --only receive data on node 0
+  return nodeId == 0
+end
+
+function onValidDataReceived(data, dataType, nodeId)
+  if storage.state then
+    logInfo(dataType .. ": " .. data)
   end
+end
+
+function name(newName)
+  storage.name = newName
 end
