3639f6f985f98ddb6daf9a68ae2e6409c0d5be5a
Alex Lawson <cheswick13@hotmail.com>
2014-01-18 00:22:32 -0800

    added placeholder object and recipe for powered smelter

--- objects/wired/energy/powersmelter/powersmelter.lua
+++ objects/wired/energy/powersmelter/powersmelter.lua
@@ -2,30 +2,25 @@ function init(virtual)
   if not virtual then
     energy.init()
     datawire.init()
-
-    entity.setInteractive(true)
+    pipes.init({itemPipe})
+    --TODO: set up storage api
   end
 end
 
 function die()
   energy.die()
+  --TODO: cleanup storage api
 end
 
 function onInteraction(args)
   storage.state = not storage.state
 end
 
---never accept energy from elsewhere
-function onEnergyNeedsCheck()
-  return 0
-end
-
 function main()
-  if storage.state then
-    -- yes, it really is that easy. uses the energyGenerationRate config parameter
-    energy.generateEnergy()
+  if storage.state and energy.consumeEnergy() then
+    -- smelt stuff
   end
-
   energy.update()
   datawire.update()
+  pipes.update(entity.dt())
 end
