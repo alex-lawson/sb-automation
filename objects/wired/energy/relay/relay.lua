3b17c729adc3b219f26d53d9b7b8ecd4f1f6bde6
Alex Lawson <cheswick13@hotmail.com>
2014-01-22 15:04:02 -0800

    improved energy pathing algorithm

--- objects/wired/energy/relay/relay.lua
+++ objects/wired/energy/relay/relay.lua
@@ -8,6 +8,10 @@ function die()
   energy.die()
 end
 
+function energy.isRelay()
+  return true
+end
+
 function onEnergyNeedsCheck(energyNeeds)
   energyNeeds[tostring(entity.id())] = -1 -- -1 is just a hack to mark relays for ordering
   return energy.energyNeedsQuery(energyNeeds)
