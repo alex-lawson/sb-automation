cec93120f9cf9afaa25210c36d56c53c42b29c2a
Xspeed <Xspeed@192.168.1.103>
2014-01-24 14:48:08 +0100

    THE PROFILER IS DONE

--- scripts/profilerapi.lua
+++ scripts/profilerapi.lua
@@ -4,27 +4,6 @@ profilerApi = {}
 function profilerApi.init()
   if profilerApi.isInit then return end
   profilerApi.hooks = {}
-  if entity ~= nil then
-    for k,v in pairs(entity) do
-      if type(v) == "function" then
-        profilerApi.hook(entity, "entity.", v, k)
-      end
-    end
-  end
-  if world ~= nil then
-    for k,v in pairs(world) do
-      if type(v) == "function" then
-        profilerApi.hook(world, "world.", v, k)
-      end
-    end
-  end
-  if tech ~= nil then
-    for k,v in pairs(tech) do
-      if type(v) == "function" then
-        profilerApi.hook(tech, "tech.", v, k)
-      end
-    end
-  end
   profilerApi.hookAll("", _ENV)
   profilerApi.isInit = true
 end
@@ -32,12 +11,12 @@ end
 --- Prints all collected data into the log ordered by total time descending
 function profilerApi.logData()
   local arr = {}
-  for k in pairs(t) do table.insert(arr, t) end
+  for k in pairs(profilerApi.hooks) do table.insert(arr, k) end
   table.sort(arr, profilerApi.sortHelp)
-  for k in ipairs(arr) do
+  for i,k in ipairs(arr) do
     local hook = profilerApi.hooks[k]
-    if hook.c > 0 then
-      world.logInfo(k .. ": total " .. hook.t .. ", count " .. hook.c .. ", avg " .. hook.a .. ", last " .. hook.e)
+    if hook.t > 0 then
+      world.logInfo(string.format("%30s: total %.15f, cnt %5i, avg %.15f, last %.15f", k, hook.t, hook.c, hook.a, hook.e))
     end
   end
 end
@@ -46,12 +25,21 @@ function profilerApi.sortHelp(e1, e2)
   return profilerApi.hooks[e1].t > profilerApi.hooks[e2].t
 end
 
+function profilerApi.canHook(fn)
+  local un = { "pairs", "ipairs", "type", "next", "assert", "error", "print", "setmetatable", "select", "rawset", "rawlen", "pcall" }
+  for i,v in ipairs(un) do
+    if v == fn then return false end
+  end
+  return true
+end
+
 function profilerApi.hookAll(tn, to)
-  if (tn == "entity.") or (tn == "world.") or (tn == "tech.") then return end
-  if tn == "profilerApi." then return end
+  if (tn == "profilerApi.") or (tn == "table.") or (tn == "coroutine.") or (tn == "os.") then return end
   for k,v in pairs(to) do
     if type(v) == "function" then
-      profilerApi.hook(to, tn, v, k)
+      if (tn ~= "") or profilerApi.canHook(k) then
+        profilerApi.hook(to, tn, v, k)
+      end
     elseif type(v) == "table" then
       profilerApi.hookAll(tn .. k .. ".", v)
     end
@@ -59,22 +47,24 @@ function profilerApi.hookAll(tn, to)
 end
 
 function profilerApi.getTime()
-  return os.time()
+  return os.clock()
 end
 
 function profilerApi.hook(to, tn, fo, fn)
   local full = tn .. fn
   profilerApi.hooks[full] = { s = -1, f = fo, e = -1, t = 0, a = 0, c = 0 }
-  to[fn] = function(...) profilerApi.hooked(full, ...) end
+  to[fn] = function(...) return profilerApi.hooked(full, ...) end
+  world.logInfo("Hooked " .. full)
 end
 
 function profilerApi.hooked(n, ...)
   local hook = profilerApi.hooks[n]
   hook.s = profilerApi.getTime()
-  hook.f(...)
+  local ret = hook.f(...)
   hook.e = profilerApi.getTime() - hook.s
   hook.t = hook.t + hook.e
   hook.a = hook.a * hook.c + hook.e
   hook.c = hook.c + 1
   hook.a = hook.a / hook.c
+  return ret
 end
