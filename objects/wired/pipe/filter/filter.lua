a1a98d2f27ae1c3cf158b8a4e20fc918d1d04483
Nirahiel <thenirahiel@gmail.com>
2014-02-01 19:41:33 +0100

    It's working now :)

--- objects/wired/pipe/filter/filter.lua
+++ objects/wired/pipe/filter/filter.lua
@@ -1,26 +1,30 @@
 function init(virtual)
   if not virtual then
+    pipes.init({liquidPipe,itemPipe})
+
     entity.setInteractive(true)
-
-    entity.setAnimationState("switchState", "off")
-
-    if storage.block == nil then storage.block = {} end
-    if storage.placedBlock == nil then storage.placedBlock = {} end
 
     self.connectionMap = {}
     self.connectionMap[1] = 2
     self.connectionMap[2] = 1
     self.connectionMap[3] = 4
     self.connectionMap[4] = 3
-  
-    pipes.init({liquidPipe,itemPipe})
   end
 end
 
 --------------------------------------------------------------------------------
 
 function onInteraction(args)
-
+  local itemName = world.entityHandItem(args.sourceId, "primary")
+  if itemName == nil then
+    if storage.itemName == nil then
+      return { "ShowPopup", { message = "Filtered item : " .. "none" }}
+    else
+      return { "ShowPopup", { message = "Filtered item : " .. storage.itemName }}
+    end
+  else
+    storage.itemName = itemName
+  end
 end
 
 --------------------------------------------------------------------------------
@@ -65,37 +69,53 @@ function onLiquidPut(liquid, nodeId)
 end
 
 function beforeItemPut(item, nodeId)
-  -- if storage.state then
-  --   --world.logInfo("passing item peek from %s to %s", nodeId, self.connectionMap[nodeId])
-  --   return peekPushItem(self.connectionMap[nodeId], item)
-  -- else
+  world.logInfo("Testing to put item %s with filter %s", item.name, storage.itemName)
+  if storage.itemName ~= nil then
+    if item.name == storage.itemName then
+      --world.logInfo("passing item peek from %s to %s", nodeId, self.connectionMap[nodeId])
+      return peekPushItem(self.connectionMap[nodeId], item)
+    end
+  else
     return false
-  -- end
+  end
 end
 
 function onItemPut(item, nodeId)
-  -- if storage.state then
-  --   --world.logInfo("passing item from %s to %s", nodeId, self.connectionMap[nodeId])
-  --   return pushItem(self.connectionMap[nodeId], item)
-  -- else
+  world.logInfo("Trying to put item %s with filter %s", item.name, storage.itemName)
+  if storage.itemName ~= nil then
+    if item.name == storage.itemName then
+      --world.logInfo("passing item from %s to %s", nodeId, self.connectionMap[nodeId])
+      return pushItem(self.connectionMap[nodeId], item)
+    end
+  else
     return false
-  -- end
+  end
 end
 
 function beforeItemGet(filter, nodeId)
-  -- if storage.state then
-  --   --world.logInfo("passing item peek get from %s to %s", nodeId, self.connectionMap[nodeId])
-  --   return peekPullItem(self.connectionMap[nodeId], filter)
-  -- else
+  if storage.itemName ~= nil then
+    for filterString, amount  in pairs(filter) do
+      local pullFilter = {}
+      if filterString == storage.itemName then
+        pullFilter[filterString] = amount
+        --world.logInfo("passing item peek get from %s to %s", nodeId, self.connectionMap[nodeId])
+        return peekPullItem(self.connectionMap[nodeId], pullFilter)
+      end
+    end
+  else
     return false
-  -- end
+  end
 end
 
 function onItemGet(filter, nodeId)
-  -- if storage.state then
-  --   --world.logInfo("passing item get from %s to %s", nodeId, self.connectionMap[nodeId])
-  --   return pullItem(self.connectionMap[nodeId], filter)
-  -- else
-    return false
-  -- end
+  if storage.itemName ~= nil then
+    for filterString, amount in pairs(filter) do
+      local pullFilter = {}
+      if filterString == storage.itemName then
+        pullFilter[filterString] = amount
+        --world.logInfo("passing item get from %s to %s", nodeId, self.connectionMap[nodeId])
+        return pullItem(self.connectionMap[nodeId], pullFilter)
+      end
+    end
+  end
 end
