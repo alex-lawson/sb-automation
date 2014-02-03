9384914a491204b3c89be84e6bb203ac12ca983c
Xspeed <Xspeed@X-PC>
2014-02-03 19:31:40 +0100

    Finished item drone and states code

--- monsters/itemdrone/behavior.lua
+++ monsters/itemdrone/behavior.lua
@@ -1,49 +1,41 @@
-function init(args)
+function init()
   entity.setDeathParticleBurst("deathPoof")
   entity.setAnimationState("movement", "flying")
-  
+  if storageApi.isInit() then
+    storageApi.init({ mode = 1, space = 9, join = true, ondeath = 1 })
+  end
   local states = stateMachine.scanScripts(entity.configParameter("scripts"), "(%a+State)%.lua")
   self.state = stateMachine.create(states)
+  if storage.stationPos == nil then
+    storage.stationPos = entity.configParameter("stationPos")
+  end
+  if (self.stationId == nil) or not world.entityExists(self.stationId) then
+    local ids = world.objectQuery(storage.stationPos, 5, { order = "nearest", name = "dronestation", callScript = "droneRegister", callScriptArgs = { entity.id() } })
+    for _,v in ids do
+      self.stationId = v
+      break
+    end
+  end
 end
 
 function die()
+  world.callScriptedEntity(self.stationId or -1, "droneDeath", entity.id())
+  storageApi.die()
+end
 
+function setDead()
+  self.dead = true
+end
+
+function shouldDie()
+  return dead or not world.entityExists(self.stationId or -1)
+end
+
+function moveTo(pos, dt)
+  entity.flyTo(pos, true)
 end
 
 function main()
   local dt = entity.dt()
   self.state.update(dt)
-
-  local masterId, minionIndex, minionTimer = findMaster()
-  if masterId ~= 0 then
-    self.hadMaster = true
-
-    local angle = ((minionIndex - 1) * math.pi / 2.0) + minionTimer
-    local target = vec2.add(world.entityPosition(masterId), {
-      20.0 * math.cos(angle),
-      8.0 * math.sin(angle)
-    })
-
-    entity.flyTo(target, true)
-  else
-    self.hadMaster = false
-
-    entity.fly({0,0}, true)
-  end
-
-  util.trackTarget(30.0, 10.0)
-
-  if self.targetPosition ~= nil then
-    entity.setFireDirection({0,0}, world.distance(self.targetPosition, entity.position()))
-    entity.startFiring("plasmabullet")
-  else
-    entity.stopFiring()
-  end
-end
-
-function hasCapability(capability)
-  if capability == 'spawnedBy' then
-    return true
-  end
-  return false
 end
