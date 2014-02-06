528357a1fc4628ba1f727adda42ddf2a1af1610b
XspeedPL <xspeedpl@windowslive.com>
2014-02-06 19:10:32 +0100

    Drone Station progress 33%

--- objects/wired/entity/dronestation/dronestation.lua
+++ objects/wired/entity/dronestation/dronestation.lua
@@ -1,19 +1,15 @@
-function init(args)
+function init(v)
   entity.setInteractive(true)
   
-  if args == false then
+  if not v then
     pipes.init({itemPipe})
     
     local initInv = entity.configParameter("initialInventory")
-    if initInv and storage.sApi == nil then
+    if initInv and (storage.sApi == nil) then
       storage.sApi = initInv
     end
 
     storageApi.init({ mode = 3, capacity = 16, ondeath = 1, merge = true })
-    
-    if entity.direction() < 0 then
-      entity.setAnimationState("flipped", "left")
-    end
     
     self.pushRate = entity.configParameter("itemPushRate")
     self.pushTimer = 0
@@ -22,6 +18,20 @@ end
 
 function die()
   storageApi.die()
+end
+
+function droneRegister(eId)
+  if (self.droneId) == nil or (self.droneId == eId) then
+    self.droneId = eId
+    return true
+  end
+  return false
+end
+
+function droneDeath(eId)
+  if self.droneId == eId then
+    self.droneId = nil
+  end
 end
 
 function onInboundNodeChange(args)
@@ -50,7 +60,7 @@ function main(args)
     for node=0,1 do
       if entity.getInboundNodeLevel(node) then
         for i,item in storageApi.getIterator() do
-          local result = pushItem(node+1, item)
+          local result = pushItem(node + 1, item)
           if result == true then storageApi.returnItem(i) end --Whole stack was accepted
           if result and result ~= true then item.count = item.count - result end --Only part of the stack was accepted
           if result then break end
@@ -60,57 +70,4 @@ function main(args)
     self.pushTimer = 0
   end
   self.pushTimer = self.pushTimer + entity.dt()
-end
-
-function onItemPut(item, nodeId)
-  if item then
-    return storageApi.storeItem(item.name, item.count, item.data)
-  end
-  return false
-end
-
-function beforeItemPut(item, nodeId)
-  if item then
-    return not storageApi.isFull() -- TODO: Make this use the future function for fitting in a stack of items
-  end
-  return false
-end
-
-function onItemGet(filter, nodeId)
-  if filter then
-    for i,item in storageApi.getIterator() do
-      for filterString,amount  in pairs(filter) do
-        if item.name == filterString and item.count >= amount[1] then
-          if item.count <= amount[2] then
-            return storageApi.returnItem(i)
-          else
-            item.count = item.count - amount[2]
-            return {name = item.name, count = amount[2], data = item.data}
-          end
-        end
-      end
-    end
-  else
-    for i,item in storageApi.getIterator() do
-      return storageApi.returnItem(i)
-    end
-  end
-  return false
-end
-
-function beforeItemGet(filter, nodeId)
-  if filter then
-    for i,item in storageApi.getIterator() do
-      for filterString,amount  in ipairs(filter) do
-        if item.name == filterString and item.count >= amount[1] then
-          return true 
-        end
-      end
-    end
-  else
-    for i,item in storageApi.getIterator() do
-      return true
-    end
-  end
-  return false
 end
