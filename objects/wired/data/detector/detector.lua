1a7ea4539500ee7526bf970f58a7724afa0e51f1
Alex Lawson <cheswick13@hotmail.com>
2014-02-28 12:10:53 -0800

    cleaning up item detector (but the tags are still broken)

--- objects/wired/data/detector/detector.lua
+++ objects/wired/data/detector/detector.lua
@@ -1,62 +1,78 @@
-function init(args)
-   if not self.initialized and not args then
-      self.initialized = true
+function init(virtual)
+   if not virtual then
       entity.setInteractive(true)
-      if storage.mode == nil then
-          storage.mode = "all"
-      end
       entity.setAllOutboundNodes(false)
 
-      entity.setGlobalTag("modePart", storage.mode)
-      self.countdown = 0
+      self.modes = entity.configParameter("modes") or {"all", "owner", "player", "monster", "item", "npc"}
+      storage.currentMode = storage.currentMode or self.modes[1]
+      entity.setGlobalTag("modePart", storage.currentMode)
+
+      self.detectDuration = entity.configParameter("detectDuration") or 1
+      self.detectCooldown = 0
+
+      self.detectArea = {entity.toAbsolutePosition({-2, -2}), entity.toAbsolutePosition({2, 2})}
    end
 end
 
 function on()
    entity.setAllOutboundNodes(true)
+   self.detectCooldown = self.detectDuration
    entity.setAnimationState("detectorState", "on")
-   self.countdown = entity.configParameter("detectTickDuration")
 end
 
 function off()
    entity.setAllOutboundNodes(false)
+   self.detectCooldown = 0
    entity.setAnimationState("detectorState", "off")
 end
 
 function onInteraction(args)
-   off()
    storage.owner = world.entityName(args["sourceId"])
-   local modes = entity.configParameter("modes")
-   for k, mode in ipairs(modes) do
-      if mode == storage.mode then
-         storage.mode = modes[k+1] or "all"
-         break
-      end
-   end
-   entity.setGlobalTag("modePart", storage.mode)
+   cycleMode()
+   off()
+end
+
+function cycleMode()
+  for i, mode in ipairs(self.modes) do
+    if mode == storage.currentMode then
+      storage.currentMode = self.modes[(i % #self.modes) + 1]
+      entity.setGlobalTag("modePart", storage.currentMode)
+      return
+    end
+  end
+
+  --previous mode invalid, default to mode 1
+  storage.currentMode = self.modes[1]
+  entity.setGlobalTag("modePart", storage.currentMode)
 end
 
 function main()
-   if self.countdown == 0 then
-      local radius, entityIds, pos = entity.configParameter("detectRadius"), {}, entity.toAbsolutePosition({1,1})
-      if storage.mode == "all" then
-         entityIds = world.entityQuery(pos, radius, { notAnObject = true })
-      elseif storage.mode == "owner" then
-         for _, entityId in pairs(world.playerQuery(pos, radius, { notAnObject = true })) do
+   --stupid tags don't update properly in init()
+   entity.setGlobalTag("modePart", storage.currentMode)
+
+   if self.detectCooldown > 0 then
+      self.detectCooldown = self.detectCooldown - entity.dt()
+   else
+      local entityIds = {}
+
+      if storage.currentMode == "all" then
+         entityIds = world.entityQuery(self.detectArea[1], self.detectArea[2], { notAnObject = true })
+      elseif storage.currentMode == "owner" then
+         for _, entityId in pairs(world.playerQuery(self.detectArea[1], self.detectArea[2], { notAnObject = true })) do
             if world.entityName(entityId) == storage.owner then
                entityIds[1] = "test"
                break
             end
          end
-      elseif storage.mode == "player" then
-         entityIds = world.playerQuery(pos, radius, { notAnObject = true })
-      elseif storage.mode == "monster" then
-         entityIds = world.monsterQuery(pos, radius, { notAnObject = true })
-      elseif storage.mode == "item" then
-         entityIds = world.itemDropQuery(pos, radius, { notAnObject = true })
-      elseif storage.mode == "npc" then
-         entityIds = world.npcQuery(pos, radius, { notAnObject = true })
-      elseif storage.mode == "solar" then
+      elseif storage.currentMode == "player" then
+         entityIds = world.playerQuery(self.detectArea[1], self.detectArea[2], { notAnObject = true })
+      elseif storage.currentMode == "monster" then
+         entityIds = world.monsterQuery(self.detectArea[1], self.detectArea[2], { notAnObject = true })
+      elseif storage.currentMode == "item" then
+         entityIds = world.itemDropQuery(self.detectArea[1], self.detectArea[2], { notAnObject = true })
+      elseif storage.currentMode == "npc" then
+         entityIds = world.npcQuery(self.detectArea[1], self.detectArea[2], { notAnObject = true })
+      elseif storage.currentMode == "solar" then
          if world.lightLevel(pos) >= entity.configParameter("lightLevel") then
             entityIds[1] = "test"
          end
@@ -67,7 +83,5 @@ function main()
       else
          off()
       end
-   else
-      self.countdown = self.countdown - 1
    end
 end
